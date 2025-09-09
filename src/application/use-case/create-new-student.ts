df44836a65ae5f17b292872b02933e4165718c2a
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-09-09 20:38:59 +0000

    fix: student集約でtaskの永続化も行うのでtaskRepositoryは操作しないよう修正

--- src/application/use-case/create-new-student.ts
+++ src/application/use-case/create-new-student.ts
@@ -2,7 +2,6 @@ import type { IAssignmentRepository } from "../../domain/assignment/assignment-r
 import { CreateNewStudentWithInitialTasks } from "../../domain/domain-service/create-new-student-with-initial-tasks";
 import type { IStudentRepository } from "../../domain/student/student-repository";
 import { MailAddress } from "../../domain/student/value-object/mail-address";
-import type { ITaskRepository } from "../../domain/task/task-repository";
 
 export interface CreateNewStudentUseCaseInput {
   firstName: string;
@@ -16,7 +15,6 @@ export class CreateNewStudentUseCase {
   constructor(
     private readonly studentRepository: IStudentRepository,
     private readonly assignmentRepository: IAssignmentRepository,
-    private readonly taskRepository: ITaskRepository,
   ) {
     this.createNewStudentWithInitialTasks =
       new CreateNewStudentWithInitialTasks(this.assignmentRepository);
@@ -34,10 +32,6 @@ export class CreateNewStudentUseCase {
     );
 
     await this.studentRepository.save(student);
-
-    await Promise.all(
-      student.tasks.map((task) => this.taskRepository.save(task)),
-    );
 
     return { studentId: student.id };
   }
